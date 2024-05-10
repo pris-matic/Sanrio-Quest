41e46a696965201a126822a88a7dac6fdfd961c8
pris-matic <ddeocadizjranthony@gmail.com>
2024-05-10 20:57:36 +0800

    added projectile collision logic with walls

--- CharacterType.java
+++ CharacterType.java
@@ -5,9 +5,9 @@ import java.util.concurrent.CopyOnWriteArrayList;
 /**
 The CharacterType abstract class is used to define the 
 stats of different characters that will be defined
-throughout the game. Both enemies and the Player's
-<code> CharacterType </code> are subclasses of this abstract
-class.
+throughout the game. The player selects from three different
+<code> CharacterType </code> subclasses of this abstract
+class that will determine their stats for the current run.
 
 <p></p>
 This class also allows the movement for the weapon of choice 
@@ -35,7 +35,8 @@ of our program.
 public abstract class CharacterType {
     
     // basic stats of the chosen character
-    protected int hp, atk, def, maxHp; // stats
+    protected double hp, atk, def, maxHp; // stats
+    protected int width, height;
     protected CharacterManager cm;
     protected double x,y,rotation;
 
@@ -45,17 +46,32 @@ public abstract class CharacterType {
     // variables related to character images
     protected int updateSpriteCounter = 0; 
     protected int spriteValue = 1;
+    protected int spriteNumber = 0;
+    protected BufferedImage img;
     protected BufferedImage idle,front1,front2,back1,back2,left1,left2,right1,right2;
+    protected BufferedImage[] imageList = new BufferedImage[9];
 
-    public int getHealth(){
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+    
+    public double getMaxHealth(){
+        return maxHp;
+    }
+
+    public double getHealth(){
         return hp;
     }
 
-    public int getAttack(){
+    public double getAttack(){
         return atk;
     }
 
-    public int getDefense(){
+    public double getDefense(){
         return def;
     }
 
@@ -98,7 +114,71 @@ public abstract class CharacterType {
 
     public abstract void getImages();
 
-    public abstract BufferedImage displayImage();
+    public BufferedImage useImage(){
+        return img;
+    }
+
+    public void displayImage(){
+
+        if (cm.isMovingDown()){
+            if (spriteValue == 1){
+                img = front1;
+                spriteNumber = 1;
+            }
+            if (spriteValue == 2){
+                img = front2;
+                spriteNumber = 2;
+            }
+        } else if (cm.isMovingLeft()){
+            if (spriteValue == 1){
+                img = left1;
+                spriteNumber = 3;
+            }
+            if (spriteValue == 2){
+                img = left2;
+                spriteNumber = 4;
+            }
+        } else if (cm.isMovingRight()){
+            if (spriteValue == 1){
+                img = right1;
+                spriteNumber = 5;
+            }
+            if (spriteValue == 2){
+                img = right2;
+                spriteNumber = 6; 
+            }         
+        } else if (cm.isMovingUp()){
+            if (spriteValue == 1){
+                img = back1;
+                spriteNumber = 7;
+            }
+            if (spriteValue == 2){
+                img = back2;
+                spriteNumber = 8;
+            }
+        } else {
+            img = idle;
+            spriteNumber = 0;
+        }
+
+        updateSpriteCounter ++;
+        if (updateSpriteCounter > 12){
+            if (spriteValue == 1){
+                spriteValue = 2;
+            } else {
+                spriteValue = 1;
+            }
+            updateSpriteCounter = 0;
+        }
+    }
+
+    public int getSpriteNumber(){
+        return spriteNumber;
+    }
+
+    public void setImage(int imageNum){
+        img = imageList[imageNum];
+    }
 
     public abstract void drawWeapon(Graphics2D g2d);
 
@@ -182,6 +262,20 @@ public abstract class CharacterType {
 
         public abstract void drawProjectile(Graphics2D g2d);
 
+        public boolean isCollidingWith(CharacterManager chr){
+            return !(chr.getX() + chr.getWidth() <= xPos ||
+            chr.getX() >= xPos + this.width ||
+            chr.getY() + chr.getHeight() <= yPos ||
+            chr.getY() >= yPos + this.height);
+        }
+
+        public boolean isCollidingWithWall(Walls w){
+            return !(w.getX() + w.getWidth() <= xPos ||
+            w.getX() >= xPos + this.width ||
+            w.getY() + w.getHeight() <= yPos ||
+            w.getY() >= yPos + this.height);
+        }
+
     }
 
     public abstract void attackMovement();
@@ -190,13 +284,21 @@ public abstract class CharacterType {
 
         for (int i = 0; i < projectiles.size() ; i ++){
             if (projectiles.get(i).isActive()){
+                for (Walls w : GameCanvas.getWalls()){
+                    if (projectiles.get(i).isCollidingWithWall(w)){
+                        projectiles.get(i).setActive();
+                        projectiles.get(i).setProjectileX(-5000);
+                        projectiles.get(i).setProjectileY(-5000);
+                    }
+                }
                 double x = Math.abs(projectiles.get(i).getX() - projectiles.get(i).getInitialX());
                 double y = Math.abs(projectiles.get(i).getY() - projectiles.get(i).getInitialY());
                 double pythagorean = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-                if (pythagorean >= 500){
+                if (pythagorean >= 500 && projectiles.get(i).isActive()){
                     projectiles.get(i).setActive();
                     projectiles.get(i).setProjectileX(-5000);
                     projectiles.get(i).setProjectileY(-5000);
+                    
                 }
             }
         }
